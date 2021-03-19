6d0cc0ffd8d9690b314822190d7c9fcda2569b71
Alexander Schmah <alexander.schmah@cern.ch>
2021-03-19 08:32:15 +0100

    Removed nsigma variables and other not needed variables from tracks.

--- StJetTrackEvent.h
+++ StJetTrackEvent.h
@@ -75,11 +75,11 @@ private:
     TLorentzVector TLV_Particle_prim; // Lorentz vector properties of this particle (primary track)
 
     Float_t        dca_to_prim; // distance of closest approach to mother particle decay vertex (or primary vertex)
-    Float_t        Particle_m2;
-    Float_t        Particle_nSigmaPi;
-    Float_t        Particle_nSigmaK;
-    Float_t        Particle_nSigmaP;
-    Float_t        Particle_qp;
+    //Float_t        Particle_m2;
+    //Float_t        Particle_nSigmaPi;
+    //Float_t        Particle_nSigmaK;
+    //Float_t        Particle_nSigmaP;
+    //Float_t        Particle_qp;
 
     UShort_t       status; // status of track: bit 0: ITS refit, bit1: TPC refit
     Float_t        TPCchi2; // TPC chi2
@@ -89,8 +89,9 @@ private:
 
 public:
     StJetTrackParticle() :
-        TLV_Particle_prim(),dca_to_prim(-1),Particle_m2(-1),Particle_nSigmaPi(-1),
-        Particle_nSigmaK(-1),Particle_nSigmaP(-1),Particle_qp(-1),status(-3),TPCchi2(-3),
+        TLV_Particle_prim(),dca_to_prim(-1),
+        //Particle_m2(-1),Particle_nSigmaPi(-1),Particle_nSigmaK(-1),Particle_nSigmaP(-1),Particle_qp(-1),
+        status(-3),TPCchi2(-3),
         TPCdEdx(-3),TOFsignal(-3),Track_length(-3)
     {
 
@@ -102,11 +103,11 @@ public:
 
         // setters
         void set_dca_to_prim(Float_t f)                     { dca_to_prim = f;            }
-        void set_Particle_m2(Float_t f)                     { Particle_m2 = f;            }
-        void set_Particle_nSigmaPi(Float_t f)               { Particle_nSigmaPi = f;      }
-        void set_Particle_nSigmaK(Float_t f)                { Particle_nSigmaK = f;       }
-        void set_Particle_nSigmaP(Float_t f)                { Particle_nSigmaP = f;       }
-        void set_Particle_qp(Float_t f)                     { Particle_qp = f;            }
+        //void set_Particle_m2(Float_t f)                     { Particle_m2 = f;            }
+        //void set_Particle_nSigmaPi(Float_t f)               { Particle_nSigmaPi = f;      }
+        //void set_Particle_nSigmaK(Float_t f)                { Particle_nSigmaK = f;       }
+        //void set_Particle_nSigmaP(Float_t f)                { Particle_nSigmaP = f;       }
+        //void set_Particle_qp(Float_t f)                     { Particle_qp = f;            }
         void set_TLV_Particle_prim(TLorentzVector tlv)      { TLV_Particle_prim = tlv;    }
 
         void setStatus(UShort_t s)                          { status = s;                 }
@@ -118,11 +119,11 @@ public:
 
         // getters
         Float_t get_dca_to_prim()              const        { return dca_to_prim;         }
-        Float_t get_Particle_m2 ()             const        { return Particle_m2;         }
-        Float_t get_Particle_nSigmaPi()        const        { return Particle_nSigmaPi;   }
-        Float_t get_Particle_nSigmaK()         const        { return Particle_nSigmaK;    }
-        Float_t get_Particle_nSigmaP()         const        { return Particle_nSigmaP;    }
-        Float_t get_Particle_qp()              const        { return Particle_qp;         }
+        //Float_t get_Particle_m2 ()             const        { return Particle_m2;         }
+        //Float_t get_Particle_nSigmaPi()        const        { return Particle_nSigmaPi;   }
+        //Float_t get_Particle_nSigmaK()         const        { return Particle_nSigmaK;    }
+        //Float_t get_Particle_nSigmaP()         const        { return Particle_nSigmaP;    }
+        //Float_t get_Particle_qp()              const        { return Particle_qp;         }
         TLorentzVector get_TLV_Particle_prim() const        { return TLV_Particle_prim;   }
 
         UShort_t getStatus()                   const        { return status;              }
